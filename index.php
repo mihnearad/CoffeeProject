b566556ed8380a9df8914a40d0ef135788b65bcc
mihnearad <106181635+mihnearad@users.noreply.github.com>
2022-06-07 18:12:39 +0300

    Style Changes
    Styled differently some parts and integrated additional button to link to results page.

--- index.php
+++ index.php
@@ -3,29 +3,35 @@
 <head>
 
 	<title>Please rate your coffee</title>
-	
+<style>
+
+h1   {color: black;font-size:25px}
+
+</style>
 </head>
 <body>
 
 <body style="background-color:#E8E8E8;">
 
+
 	<left>
 		<h1>Please rate your coffee</h1>
+
 		<form action="insert.php" method="post">
-			
+
 <p>
 			<label for="Date">Date:</label>
 			<input type="date" name="Date" id="Date" required>
 			</p>
 
-			
+
 <p>
 			<label for="Time">Time:</label>
 			<input type="TIME" name="Time" id="Time" required>
 			</p>
 
-	
-			
+
+
 <p>
 			<label for="CoffeeType">Choose a Coffee Type:</label>
 			<select name="CoffeeType" id="CoffeeType">
@@ -42,13 +48,13 @@
 			</select>
 			</p>
 
-			
+
 <p>
 			<label for="Beans">Beans:</label>
 			<input type="decimal" name="Beans" id="Beans">
 			</p>
 
-			
+
 <p>
 			<label for="TimeBrew">Time Brewed:</label>
 			<input type="decimal" name="TimeBrew" id="TimeBrew">
@@ -63,7 +69,7 @@
 			<label for="Acidity">Acidity</label>
 			<input type="number" name="Acidity" id="Acidity">
 			</p>
-			
+
 <p>
 			<label for="Comments">Comments</label>
 			<input type="text" name="Comments" id="Comments">
@@ -74,22 +80,32 @@
 			<label for="Grade2">Taste Grade:</label>
 			<input type="number" name="Grade" id="Grade">
 			</p>
-			
+
 			<p>
 			<label for="User">Choose an User:</label>
 			<select name="User" id="User">
 				<option value="1">Mihnea</option>
 				<option value="2">Dasha</option>
+				<option value="3">Other</option>
 			</select>
 			</p>
 
 
 			<input
 
-			
-
 			type="submit" style=background-color:white;  value="Submit" >
+
+
 		</form>
+
+<p>
+	<a href ="results.php">
+		<button type="button" style=background-color:white >Results</button>
+	</a>
+</p>
+
+
+
 	</center>
 </body>
 </html>
